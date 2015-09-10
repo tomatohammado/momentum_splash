15a8e19957eb0b47a31d230e6cefccf25ed1da43
Hammad Malik <hmalik@repequity.com>
2015-09-10 16:54:37 -0400

    alright - got js to set the container height. Downside - resizing isn't firing the function again...for now!

--- js/momentum.js
+++ js/momentum.js
@@ -1,5 +1,17 @@
+
+// var viewportHeight = $(window).innerHeight()
+// var setHeight = $(".page-wrapper").height(viewportHeight);
+var setHeight = $(".page-wrapper").css("height", $(window).innerHeight());
+
 $(document).ready(function() {
 
   console.log('jQuery ' + $().jquery + ' is firing.');
+  setHeight;
 
 });
+
+// $(window).resize(function() {
+//     // console.log(viewportHeight);
+//     setHeight;
+// });
+
