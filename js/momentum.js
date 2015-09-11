ca8be0cb0f5169da51e26dbfad182f6fafb93a26
Hammad Malik <hmalik@repequity.com>
2015-09-11 14:54:42 -0400

    js - now with frame width variable!!!

--- js/momentum.js
+++ js/momentum.js
@@ -1,19 +1,21 @@
-var setHeight = function() {
+var frameWidth = 80;
+
+var resizer = function() {
   //window, viewport, what's the right _name_ for it? also (research) http://www.matanich.com/2013/01/07/viewport-size/
   // var viewHeight = $(window).innerHeight();
 
   $(".page-wrapper").css("height", $(window).innerHeight());
-  $(".md-modal").css("height", ($(window).innerHeight() - 80));
-  $(".md-modal").css("width", ($(window).innerWidth() - 80));
+  $(".md-modal").css("height", ($(window).innerHeight() - frameWidth));
+  $(".md-modal").css("width", ($(window).innerWidth() - frameWidth));
 }
 
 $(document).ready(function() {
   console.log('jQuery ' + $().jquery + ' is firing.');
-  setHeight();
+  resizer();
 });
 
 $(window).resize(function() {
-    setHeight();
+    resizer();
 });
 
 
