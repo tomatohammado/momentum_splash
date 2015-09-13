98461bad49ef4210b91c029e0c2f79630a5741b1
Hammad Malik <hmd.node@gmail.com>
2015-09-13 04:47:28 -0400

    better variable use

--- js/momentum.js
+++ js/momentum.js
@@ -1,12 +1,12 @@
-var frameWidth = 80;
+var frameWidth = 40;
 
 var resizer = function() {
   //window, viewport, what's the right _name_ for it? also (research) http://www.matanich.com/2013/01/07/viewport-size/
   // var viewHeight = $(window).innerHeight();
 
   $(".page-wrapper").css("height", $(window).innerHeight());
-  $(".md-modal").css("height", ($(window).innerHeight() - frameWidth));
-  $(".md-modal").css("width", ($(window).innerWidth() - frameWidth));
+  $(".md-modal").css("height", ($(window).innerHeight() - frameWidth*2));
+  $(".md-modal").css("width", ($(window).innerWidth() - frameWidth*2));
 }
 
 $(document).ready(function() {
